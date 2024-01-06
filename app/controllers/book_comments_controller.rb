a923f08aa7cac757da77215c184cfba0b760e979
ystm08 <150111719+ystm08@users.noreply.github.com>
2024-01-06 15:21:32 +0000

    「Finish]フォロー機能追加

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,7 +2,8 @@ class BookCommentsController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
+    comment = BookComment.new(book_comment_params)
+    comment.user_id = current_user.id
     comment.book_id = book.id
     comment.save
     redirect_to request.referer
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,23 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   has_one_attached :profile_image
+
+  def follow(user_id)
+    active_relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    active_relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
